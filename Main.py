2abc0df14edf7a6d3a3ed1d628930eff9809ce05
pierre-grd <79649035+pierre-grd@users.noreply.github.com>
2022-04-29 15:31:57 +0200

    add z

--- Main.py
+++ Main.py
@@ -4,8 +4,8 @@ x = "new number"
 
 path = os.path.join("C:/Users/pierr/Documents/GitHub/QARM", "Data QARM.xlsx")
 
-y = pd.read_excel(path, engine="openpyxl", sheet_name="CO2 Emissions")
+#y = pd.read_excel(path, engine="openpyxl", sheet_name="CO2 Emissions")
+z = pd.read_csv("/Users/defalt/Documents/GitHub/QARM/Market cap.csv", on_bad_lines="skip")
 
-print(y)
-
+print(z)
 
