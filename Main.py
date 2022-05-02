b85044fe8f2966619258cc4bfe269875dd0e42f3
Ke4594 <104622872+Ke4594@users.noreply.github.com>
2022-05-02 16:38:09 +0200

    02.05 koker

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ x = "new number"
 
 path = os.path.join("C:/Users/pierr/Documents/GitHub/QARM", "Data QARM.xlsx")
 
-y = pd.read_excel("/Users/defalt/Documents/GitHub/QARM/Data QARM.xlsx", engine="openpyxl", sheet_name="CO2 Emissions")
+y = pd.read_excel("Data QARM.xlsx", engine="openpyxl", sheet_name="CO2 Emissions")
 #u = pd.read_csv("/Users/defalt/Documents/GitHub/QARM/Market cap.csv", on_bad_lines="skip", header=0)
 
 print(y.iloc[1:1])
