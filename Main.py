242b5db226b9eec2a3bba1f8127c24690099333f
Ke4594 <104622872+Ke4594@users.noreply.github.com>
2022-05-03 16:12:33 +0200

    ok

--- Main.py
+++ Main.py
@@ -9,28 +9,35 @@ x = "new number"
 
 market_cap = pd.read_excel("Data QARM-2.xlsx", engine="openpyxl", sheet_name="Market Cap")
 # y = pd.read_excel("Data QARM.xlsx", engine="openpyxl", sheet_name="CO2 Emissions")
-
+feuille1 = pd.read_excel("Data QARM-2.xlsx", engine="openpyxl", sheet_name="Feuille 1 - Group_P")
 revenue = pd.read_excel("Data QARM-2.xlsx", engine="openpyxl", sheet_name="Revenue")
 sic = pd.read_excel("Data QARM-2.xlsx", engine="openpyxl", sheet_name="SIC")
 
-market_cap = market_cap.merge(sic)
-print(market_cap)
+#market_cap = market_cap.merge(sic)
+#print(market_cap)
 # tt_return_index = pd.read_excel("Data QARM.xlsx", engine="openpyxl", sheet_name="TT Return Index")
 #for i in range(1, 149):
   #print(revenue.iloc[i, 1:277].describe())
 
 #150*278
 
-print(len(revenue.columns))
+print(feuille1)
+
+#market_cap = market_cap.merge(feuille1)
+
+# print(y.iloc[::1])
+
+# Question 2 - KC TRY --------------------
+
+#revenue = revenue.dropna()
+#print(market_cap)
 
 
-<<<<<<< HEAD
-
-print(y.iloc[::1])
-
-# Question 2
 
 
 
-=======
->>>>>>> 0731837c0533c7a4f32e51c9bb894a76262d7de1
+
+# Question 3 -----------------------
+
+
+
